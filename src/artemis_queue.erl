237ea686c3840e41180adcd514c6998999b6a386
Sam Rijs <srijs@airpost.net>
2014-09-02 01:41:01 +1000

    queue: implement pop and use queue module

--- src/artemis_queue.erl
+++ src/artemis_queue.erl
@@ -6,13 +6,15 @@
 -export([handle_info/2, terminate/2, code_change/3]).
 
 % simple public interface
--export([start/0, put/1, peek/0]).
+-export([start/0, put/1, pop/0, peek/0]).
 
 
 
 start() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 put(Job) -> gen_server:call(?MODULE, {put, Job}).
+
+pop() -> gen_server:call(?MODULE, {pop}).
 
 peek() -> gen_server:call(?MODULE, {peek}).
 
@@ -25,6 +27,11 @@ handle_call({put, Job}, _, State) ->
   case insert_job(Job, State) of
     exists   -> {reply, exists,  State};
     NewState -> {reply, success, NewState}
+  end;
+handle_call({pop}, _, State) ->
+  case take_job(State) of
+    empty           -> {reply, empty, State};
+    {Job, NewState} -> {reply, Job, NewState}
   end;
 handle_call({peek}, _, State) ->
   case peek_job(State) of
@@ -44,11 +51,50 @@ terminate(_, _) ->
 code_change(_, State, _) ->
   {ok, State}.
 
+
+% peek at the next job in the queue
+
 peek_job({_, Tree}) ->
   case gb_trees:is_empty(Tree) of
     true  -> empty;
-    false -> {_, [Job|_]} = gb_trees:smallest(Tree), Job
+    false -> peek_job_nonempty(Tree)
   end.
+
+peek_job_nonempty(Tree) ->
+  {_, Queue} = gb_trees:smallest(Tree),
+  queue:get(Queue).
+
+
+% pop the next job from the queue
+
+take_job({Dict, Tree}) ->
+  Result = case gb_trees:is_empty(Tree) of
+    true -> empty;
+    false -> take_job_nonempty(Tree)
+  end,
+  case Result of
+    empty -> empty;
+    {Job, Queue} -> {Job, {delete_job_from_dict(Job, Dict),
+                           delete_job_from_tree(Job, Queue, Tree)}}
+  end.
+
+take_job_nonempty(Tree) ->
+  {_, Queue} = gb_trees:smallest(Tree),
+  case queue:out(Queue) of
+    {{value, Job}, NewQueue} -> {Job, NewQueue}
+  end.
+
+delete_job_from_tree({_, Prio, _, _}, Queue, Tree) ->
+  case queue:is_empty(Queue) of
+    true -> gb_trees:delete(Prio, Tree);
+    false -> gb_trees:update(Prio, Queue, Tree)
+  end.
+
+delete_job_from_dict({Id, _, _, _}, Dict) ->
+  dict:erase(Id, Dict).
+
+
+% insert a job into the queue
 
 insert_job(Job = {Id, _, _, _}, {Dict, Tree}) ->
   case dict:is_key(Id, Dict) of
@@ -61,8 +107,8 @@ insert_job_into_dict(Job = {Id, _, _, _}, Dict) ->
   dict:store(Id, Job, Dict).
 
 insert_job_into_tree(Job = {_, Prio, _ ,_}, Tree) ->
-  List = case gb_trees:lookup(Prio, Tree) of
-    none  -> [];
-    [H|T] -> [H|T]
+  Jobs = case gb_trees:lookup(Prio, Tree) of
+    none -> queue:new();
+    {value, Queue} -> Queue
   end,
-  gb_trees:enter(Prio, [Job|List], Tree).
+  gb_trees:enter(Prio, queue:in(Job, Jobs), Tree).
