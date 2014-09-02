58984adfec956a2584d134532f23fd03b4e4dc28
Sam Reis <sreis@atlassian.com>
2014-09-02 17:36:32 +1000

    queue: add get command

--- src/artemis_queue.erl
+++ src/artemis_queue.erl
@@ -6,13 +6,15 @@
 -export([handle_info/2, terminate/2, code_change/3]).
 
 % simple public interface
--export([start/0, put/1, pop/0, peek/0]).
+-export([start/0, put/1, get/1, pop/0, peek/0]).
 
 
 
 start() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 put(Job) -> gen_server:call(?MODULE, {put, Job}).
+
+get(Id) -> gen_server:call(?MODULE, {get, Id}).
 
 pop() -> gen_server:call(?MODULE, {pop}).
 
@@ -28,15 +30,20 @@ handle_call({put, Job}, _, State) ->
     exists   -> {reply, exists,  State};
     NewState -> {reply, success, NewState}
   end;
+handle_call({get, Id}, _, State) ->
+  case get_job(Id, State) of
+    error -> {reply, error, State};
+    Job   -> {reply, Job,   State}
+  end;
 handle_call({pop}, _, State) ->
   case take_job(State) of
     empty           -> {reply, empty, State};
-    {Job, NewState} -> {reply, Job, NewState}
+    {Job, NewState} -> {reply, Job,   NewState}
   end;
 handle_call({peek}, _, State) ->
   case peek_job(State) of
     empty -> {reply, empty, State};
-    Job   -> {reply, Job, State}
+    Job   -> {reply, Job,   State}
   end.
 
 handle_cast(_, State) ->
@@ -69,11 +76,11 @@ peek_job_nonempty(Tree) ->
 
 take_job({Dict, Tree}) ->
   Result = case gb_trees:is_empty(Tree) of
-    true -> empty;
+    true  -> empty;
     false -> take_job_nonempty(Tree)
   end,
   case Result of
-    empty -> empty;
+    empty        -> empty;
     {Job, Queue} -> {Job, {delete_job_from_dict(Job, Dict),
                            delete_job_from_tree(Job, Queue, Tree)}}
   end.
@@ -86,12 +93,21 @@ take_job_nonempty(Tree) ->
 
 delete_job_from_tree({_, Prio, _, _}, Queue, Tree) ->
   case queue:is_empty(Queue) of
-    true -> gb_trees:delete(Prio, Tree);
+    true  -> gb_trees:delete(Prio, Tree);
     false -> gb_trees:update(Prio, Queue, Tree)
   end.
 
 delete_job_from_dict({Id, _, _, _}, Dict) ->
   dict:erase(Id, Dict).
+
+
+% retrieve a job by id
+
+get_job(Id, {Dict, _}) ->
+  case dict:find(Id, Dict) of
+    {ok, Job} -> Job;
+    error     -> error
+  end.
 
 
 % insert a job into the queue
@@ -108,7 +124,7 @@ insert_job_into_dict(Job = {Id, _, _, _}, Dict) ->
 
 insert_job_into_tree(Job = {_, Prio, _ ,_}, Tree) ->
   Jobs = case gb_trees:lookup(Prio, Tree) of
-    none -> queue:new();
+    none           -> queue:new();
     {value, Queue} -> Queue
   end,
   gb_trees:enter(Prio, queue:in(Job, Jobs), Tree).
